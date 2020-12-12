76487323fc38b2ab85eecddb70d1f633602cb8ef
mateomh <mmojicah.84@gmail.com>
2020-12-11 22:19:04 -0500

    Create a mock for the person display component

---
+++ src/components/persondisplay.js
@@ -0,0 +1,12 @@
+/* eslint-disable react/self-closing-comp */
+/* eslint-disable arrow-body-style */
+const PersonDisplay = () => {
+  return (
+    <div className="container">
+      <div className="information"></div>
+      <div className="vote-tracking"></div>
+    </div>
+  );
+};
+
+export default PersonDisplay;
